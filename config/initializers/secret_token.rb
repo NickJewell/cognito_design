7d94c405fd254057d84f95ba053277727176b7ef
Nick Jewell <nick.jewell@gmail.com>
2011-09-28 20:55:37 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cognito1::Application.config.secret_token = '9f92f7d411c2133fbb6ae279e6ba567f9dbe5781edb0e3cc81ebd14fbed98a86f3060a3d0d218188f8a931197279e3a4c49b3347cf8f7ce177bffb50b1e24d5f'
