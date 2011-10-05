42905ee2e614fb22678e3a78cbe6e957dfba7f64
Nick Jewell <nick.jewell@gmail.com>
2011-10-05 21:09:03 +0100

    Added a Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Cognito1::Application.routes.draw do
+  get "users/new"
+
   match '/about', :to => 'pages#about'
   match '/contact', :to => 'pages#contact'
   match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
   
   root :to => 'pages#home'
 
