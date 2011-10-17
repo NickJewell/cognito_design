93b189f34dcb70b096784d1e2d1566fbda21c8ea
Nick Jewell <nick.jewell@gmail.com>
2011-10-17 21:06:21 +0100

    Finished the user screens

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,16 @@ class UsersController < ApplicationController
     @title = "Sign Up"
     @user = User.new
   end
+  
+  def create
+    @user = User.new (params[:user])
+    if @user.save
+      redirect_to @user, :flash => { :success => "Welcome to the Sample App! "}
+    else    
+      @title = "Sign Up"
+      render 'new'
+    end
+  end
 
 
 end
