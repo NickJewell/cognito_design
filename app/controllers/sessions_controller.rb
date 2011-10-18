c5c07b5346967eb35ab6f83cec1d4f157963d19b
Nick Jewell <nick.jewell@gmail.com>
2011-10-18 22:45:16 +0100

    Completed signin and signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,12 +13,13 @@ class SessionsController < ApplicationController
       render 'new'
     else 
       sign_in user
-      redirect_to user_path 
+      redirect_to user
     end
   end
   
   def destroy
-    
+    sign_out
+    redirect_to root_path
   end
   
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,7 +6,6 @@ module SessionsHelper
   
   def current_user=(user)
     @current_user = user
-    
   end
   
   def current_user
@@ -15,6 +14,11 @@ module SessionsHelper
   
   def signed_in?
     !current_user.nil? 
+  end
+  
+  def sign_out
+    cookies.delete(:remember_token)
+    self.current_user = nil
   end
   
   private
