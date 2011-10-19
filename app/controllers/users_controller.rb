c6e2ab8162674e8c7fd4e8e34935f1abad49bf10
Nick Jewell <nick.jewell@gmail.com>
2011-10-19 21:31:24 +0100

    Added a per page limit to the users index pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def index
     @title = "All Users"
-    @users = User.paginate(:page => params[:page])
+    @users = User.paginate(:page => params[:page], :per_page => 5)
   end
 
   def show
