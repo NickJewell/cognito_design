bf5c161c8c7664b3b16076d8ce722b25350735d5
Nick Jewell <nick.jewell@gmail.com>
2011-10-20 22:26:05 +0100

    Can now destroy users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => [:destroy]
 
   def index
     @title = "All Users"
@@ -43,7 +44,16 @@ class UsersController < ApplicationController
       render 'edit'  
     end
   end
+
+
+def destroy
+  User.find(params[:id]).destroy
+  flash[:success] = "User Destroyed"
+  redirect_to users_path, :flash => { :success => "User Destroyed."}
 end
+
+end
+
 
 private
   def authenticate
@@ -52,6 +62,10 @@ private
   
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_path) unless current_user?(@user)
+    redirect_to(root_path) unless current_user?(@user) 
+  end
+  
+  def admin_user
+      redirect_to((root_path), :flash => { :error => "Cannot delete admin or yourself!"}) unless current_user.admin? && current_user?(@user)
   end
 
