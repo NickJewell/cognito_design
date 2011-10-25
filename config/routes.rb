4c2d9fa292ba305fa470b6cd461192a0e85a3b0a
Nick Jewell <nick.jewell@gmail.com>
2011-10-25 22:11:05 +0100

    Microposts Interface

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Cognito1::Application.routes.draw do
 
 
   resources :users 
-  resources :sessions, :only => [:new, :create, :destroy]
+  resources :sessions,   :only => [:new, :create, :destroy]
+  resources :microposts, :only => [:create, :destroy]
 
   match '/about', :to => 'pages#about'
   match '/contact', :to => 'pages#contact'
