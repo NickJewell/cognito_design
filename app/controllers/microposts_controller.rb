9224d64dd24cf4785c01e5982b2ba729e13b52e0
Nick Jewell <nick.jewell@gmail.com>
2011-10-25 22:47:32 +0100

    Finished Posts as a basis for Events

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_filter :authenticate
+  before_filter :authorized_user, :only => :destroy
   
   
   def create
@@ -7,13 +8,22 @@ class MicropostsController < ApplicationController
     if @micropost.save
       redirect_to root_path, :flash => { :success => "Post Created"}
     else
+      @feed_items = []
       render 'pages/home'
     end
   
   end
   
   def destroy
-    
+    @micropost.destroy
+    redirect_to root_path, :flash => { :success => "Post Deleted" }
+  end
+  
+  private
+  
+  def authorized_user
+    @micropost = Micropost.find(params[:id])
+    redirect_to root_path unless current_user?(@micropost.user)
   end
   
 end
