bda63f4f34a2d30a50f6e976c225a524ba7b94bb
Nick Jewell <nick.jewell@gmail.com>
2011-11-13 22:17:14 +0000

    Got collection dropdowns to work in event's show view...progress

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,13 @@ class EventsController < ApplicationController
   def show
     @title = "All Events - Show"
     # Deliberately blank for now...
+    @events             = Event.find(params[:id])
+    @title              = @events.title
+    @channels           = Channel.find(:all, :order => "id")
+    @difficulties       = Difficulty.find(:all, :order => "id")
+    @sizes              = Size.find(:all, :order => "id")
+    @statuses           = Status.find(:all, :order => "id")
+    @timezones          = Timezone.find(:all, :order => "id")
   end
   
 
