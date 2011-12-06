5fe4d3821a5ac6852afedf28767bca252175182a
Nick Jewell <nick.jewell@gmail.com>
2011-12-06 22:43:39 +0000

    Datepicker working, Slider still has work to go

---
+++ app/assets/javascripts/jquery_event_datepicker.js
@@ -0,0 +1,9 @@
+$(function() {
+		$(".datepicker").datepicker({
+		                dateFormat : "dd MM yy",
+		                showOn : "both",
+		                changeMonth : true,
+		                changeYear : true,
+		                yearRange : "c-1:c+5"
+		            })
+	});
---
+++ app/assets/javascripts/jquery_event_slider.js
@@ -0,0 +1,6 @@
+	$(function() {
+			$( "#slider" ).slider({value:100,
+						min: 0,
+						max: 500,
+						step: 50});
+		});
